31455457de509cc4d6e14fe267015750db78461d
Michael Toohig <michael.toohig@gmail.com>
2024-02-15 16:08:08 +1100

    WIP testing vue3-leaflet package functionality

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,13 +1,37 @@
 <template>
   <div class="about">
-    <div class="container mockup-code max-w-xs">
-      <pre data-prefix="$"><code>// This is an about page</code></pre>
-    </div>
-
+    <MapContainer :center="center" class="h-full w-full" v-on:zoomend="onMapChange" v-on:move="onMapChange">
+      <OpenStreetMap />
+      <Marker :position="[48.3151, 3.68461]" />
+      <Marker :position="[49.68561, 3.9881]">
+        <Popup>
+          <div class="text-blue-600 font-semibold">Hello world!</div>
+        </Popup>
+      </Marker>
+    </MapContainer>
   </div>
 </template>
 
+<script setup lang="ts">
+console.log('1')
+
+import { MapContainer, OpenStreetMap, Marker, Popup } from 'vue3-leaflet';
+import { inject, ref } from 'vue';
+
+const center = ref([48.3151, 3.68461]);
+
+const makeMarkers = () => {
+  // TODO produce a couple hundred random markers with tooltips etc to test
+}
+
+const onMapChange = () => {
+  makeMarkers();
+}
+</script>
+
 <style scoped lang="scss">
+@import 'leaflet/dist/leaflet.css';
+
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
